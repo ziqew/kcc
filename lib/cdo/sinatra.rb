d6260faf67073c71eda94105237603034d6b41e9
ziqew <gongwenwei@gmail.com>
2018-03-31 16:28:16 +0800

    upgrade sinatra from 2.0.0.beta2 to 2.0.0

--- lib/cdo/sinatra.rb
+++ lib/cdo/sinatra.rb
@@ -4,7 +4,7 @@ class Sinatra::Base
   # Patch Sinatra 2.x to allow anchors in regexp patterns,
   # to match 1.x behavior.
   # Ref: https://github.com/sinatra/sinatra/issues/1178
-  def self.compile(path)
+  def self.compile(path,route_mustermann_opts = {})
     opts = path.is_a?(Regexp) ? {check_anchors: false} : {}
     Mustermann.new(path, opts)
   end
